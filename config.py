4eaba250629eda2562059ea812daab25525d9620
Yusuf580 <yusufandrew580@gmail.com>
2026-04-02 19:23:26 +0300

    Feat:Errors in production

--- config.py
+++ config.py
@@ -1,17 +1,16 @@
 import os
 from dotenv import load_dotenv
 
-# Load variables from .env
 load_dotenv()
 
 class Config:
-    DB_USER = os.getenv("DB_USER")
+    DB_USER     = os.getenv("DB_USER")
     DB_PASSWORD = os.getenv("DB_PASSWORD")
-    DB_HOST = os.getenv("DB_HOST")
-    DB_NAME = os.getenv("DB_NAME")
+    DB_HOST     = os.getenv("DB_HOST")
+    DB_PORT     = os.getenv("DB_PORT", "5432")
+    DB_NAME     = os.getenv("DB_NAME")
 
     SQLALCHEMY_DATABASE_URI = (
-        f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+        f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
     )
-
     SQLALCHEMY_TRACK_MODIFICATIONS = False
